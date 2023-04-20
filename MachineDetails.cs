78ec37afdba0812b85ce67eb82aa2bd0618fd133
hjal17 <hjal099@gmail.com>
2023-04-20 23:58:21 +0100

    update MachineDetail class

--- MachineDetails.cs
+++ MachineDetails.cs
@@ -10,10 +10,10 @@ namespace KafkaTemperatureProducer
     {
         public Guid Id { get; set; }
         public Guid MachineId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Temperature { get; set; }
-        public string NiveauCo2 { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Temperature { get; set; }
+        public string? NiveauCo2 { get; set; }
         public DateTime TemperatureDate { get; set; }
         public MachineDetails() { }
 
